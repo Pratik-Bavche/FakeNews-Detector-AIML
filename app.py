dff436efe1a78654de2e3e4d4260c84bf29ec18f
Pratik <pratikbavche2005@gmail.com>
2025-11-16 21:32:41 +0530

    flask initialize

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ import string
 import pandas as pd
 import os
 
-app = Flask(__name__)   
+app = Flask(__name__)   #initialize Flask app // __name__ represents where is the webpage related files are stored
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))       #get current directory path
+model_path = os.path.join(BASE_DIR, "model.pkl")            #combine current directory path with model file name
+
+print("Full Path:", model_path)
+
+#Load the model
+try:
+    Model = joblib.load(model_path)
+    print("Model loaded successfully!")
+except Exception as e:
+    print("Error loading model:", e)
